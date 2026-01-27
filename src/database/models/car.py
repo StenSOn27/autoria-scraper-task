81cd9ef694fe93468c8621bc59b3c5c39e5e3825
Taras Shevchyk <tarashevchik27@gmail.com>
2026-01-27 18:24:53 +0200

    describe models

---
+++ src/database/models/car.py
@@ -0,0 +1,27 @@
+from sqlalchemy import (
+    Integer, DateTime, String, func, BigInteger
+)
+from datetime import datetime
+from base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Car(Base):
+    __tablename__ = "cars"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    url: Mapped[str] = mapped_column(String(255))
+    title: Mapped[str] = mapped_column(nullable=False)
+    price_usd: Mapped[float] = mapped_column(Integer)
+    odometer: Mapped[int] = mapped_column(Integer)
+    username: Mapped[str] = mapped_column(String(255))
+    phone_number: Mapped[int] = mapped_column(BigInteger)
+    image_url: Mapped[str] = mapped_column(String(2048))
+    images_count: Mapped[int] = mapped_column(Integer)
+    car_number: Mapped[str] = mapped_column(String(255))
+    car_vin: Mapped[str] = mapped_column(String(255))
+    
+    datetime_found: Mapped[datetime] = mapped_column(
+        DateTime, 
+        server_default=func.now()
+    )
